22003b7a131f5b3b73292ed37da154163046ed50
Rameez Khan <rameezkhan.sa@gmail.com>
2022-01-11 11:00:35 +0200

    Add markdown-clj

--- src/recipes/core.clj
+++ src/recipes/core.clj
@@ -1,7 +1,30 @@
 (ns recipes.core
-  (:require [stasis.core :as statis]))
+  (:require [stasis.core :as statis]
+            [markdown.core :as markdown]
+            [clojure.string :as str]))
 
 (def recipes-dir "content/recipes")
+(def publish-dir "out")
+(def md-file-pattern #"^.*\.md$")
 
-(statis/slurp-directory recipes-dir #".*\.md$")
-;; => {"/neapolitan-pizza.md" "## Ingredients\n- 00 Flour\n\n## Method\n### Day 1\n- Make Poolish\n"}
+(defn key->html
+  "Convert statis markdown key to html"
+  [k]
+  (str/replace k #".md" ".html"))
+
+(defn read-and-convert!
+  "Read and convert markdown files to html"
+  []
+  (let [md-files       (statis/slurp-directory recipes-dir md-file-pattern)
+        paths          (map key->html (keys md-files))
+        html-content   (map markdown/md-to-html-string (vals md-files))]
+    (zipmap paths html-content)))
+
+(statis/empty-directory! publish-dir)
+(statis/export-pages (read-and-convert!) publish-dir)
+
+(comment
+  (read-and-convert!))
+
+
+
